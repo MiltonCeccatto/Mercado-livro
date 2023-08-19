28024bc586a52756d8901b3e349b6577aa588719
Milton <miltonjunior.pf076@academico.ifsul.edu.br>
2023-08-19 11:00:58 -0300

    Finalizando parte de testes unitários

---
+++ mercado-livro/src/test/kotlin/com/mercadolivro/event/listeners/GenerateNfeListenerTest.kt
@@ -0,0 +1,40 @@
+package com.mercadolivro.event.listeners
+
+import com.mercadolivro.event.PurchaseEvent
+import com.mercadolivro.helper.buildPurchase
+import com.mercadolivro.service.PurchaseService
+import io.mockk.*
+import io.mockk.InternalPlatformDsl.toStr
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import java.util.UUID
+
+@ExtendWith(MockKExtension::class)
+class GenerateNfeListenerTest {
+
+    @MockK
+    private lateinit var purchaseService: PurchaseService
+
+    @InjectMockKs
+    private lateinit var generateNfeListener: GenerateNfeListener
+
+    @Test
+    fun `should generate nfe`(){
+        val purchase = buildPurchase(nfe = null)
+        //mock estatico, gerando um número para que use o que nós queremos
+        val fakeNfe = UUID.randomUUID()
+        val purchaseExpected = purchase.copy(nfe = fakeNfe.toString())
+        mockkStatic(UUID::class)
+
+        every { UUID.randomUUID() } returns fakeNfe
+        every { purchaseService.update(purchaseExpected) } just runs
+
+        generateNfeListener.listen(PurchaseEvent(this, purchase))
+
+        verify(exactly = 1) { purchaseService.update(purchaseExpected) }
+    }
+}
